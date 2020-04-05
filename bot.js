5e034057decaacb21696806e411d92f18f010ba1
bagaskoro1911999 <63148858+bagaskoro1911999@users.noreply.github.com>
2020-04-05 07:32:36 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    bot.user.setStatus('Online')
+    bot.user.setGame('#OCEANROLEPLAY')
 });
 
 client.on('message', message => {
