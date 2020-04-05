68116484f83b6a8cbc964f8dfb3888e6c6dc83a5
bagaskoro1911999 <63148858+bagaskoro1911999@users.noreply.github.com>
2020-04-05 09:00:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-conts {client, Attachment} = require('discord.js)
-conts bot = new client();
+const Client = require('./structures/Client');
 
-conts PREFIX = `!`
+/* Global Options */
+let options = {
+  prefix: '?'
+}
 
-var version = `1.0.1`;
-
-bot.on('ready', () =>{
-    console.log(this bot is online!);
-    bot.user.setActivity('OCEANRP')
-    
-})
-
-bot.on('guildMemberAdd', member =>{
-
-    const channel = member.guild.channels.find(channel.name === "welcome")}
-    iƒ(!channel) return;
-
-    channel.send('welcome to our server,$(member), please red the rules in the rules channel!`
-});
+const client = new Client(options);
+client.run();
