6dc357c17315276208ebd8fce5fa743b932b4f55
bagaskoro1911999 <63148858+bagaskoro1911999@users.noreply.github.com>
2020-04-05 12:14:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-bot.on('ready', () =>{
-    console.log('Bot Launched...')
-    bot.user.setStatus('Online')
-    bot.user.setGame(OceanRP)
+const {Client, Attachment} = require('discord.js');
+const bot = new DiscordClient();
 
+const token = "Njk2MjE1ODExMzE1OTkwNjA5.Xolfqg.gUMmJK3UnLqqxsfdacbUvAEmSuI";
+
+const PREFIX = '!';
+
+var version = '1.0.1';
+
+bot.on('ready', () =>{
+    console.log('This bot is online!');
+    bot.user.setActivity('OceanRP');
+
+})
+
+bot.on('guildMemberAdd', member =>{
+    const channel = member.guild.channels.find(channel => channel.name === "welcome");
+    if(!channel) return;
+
+    channel.send('welcome to our sever${member}, please read rules in the rules channel')
 })
